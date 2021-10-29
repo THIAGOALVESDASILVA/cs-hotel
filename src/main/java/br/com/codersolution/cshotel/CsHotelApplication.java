488057d7fa7242fa33297792f530d9c239b50fd9
João Souza <j.gabrieltec@gmail.com>
2021-10-29 01:36:59 -0300

    feat: add base of application

---
+++ src/main/java/br/com/codersolution/cshotel/CsHotelApplication.java
@@ -0,0 +1,13 @@
+package br.com.codersolution.cshotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/codersolution/cshotel/CsHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codersolution.cshotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

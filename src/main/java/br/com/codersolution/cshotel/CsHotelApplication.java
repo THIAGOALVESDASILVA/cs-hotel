5f64e9d06138699e03cc798d1255725120ee9479
João Souza <j.gabrieltec@gmail.com>
2021-11-02 02:20:44 -0300

    feat: add spring config and remove application properties

--- src/main/java/br/com/codersolution/cshotel/CsHotelApplication.java
+++ src/main/java/br/com/codersolution/cshotel/CsHotelApplication.java
@@ -2,7 +2,11 @@ package br.com.codersolution.cshotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
+@EnableConfigurationProperties
 @SpringBootApplication
 public class CsHotelApplication {
 
